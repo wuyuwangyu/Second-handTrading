b5b0c5b27820470ce06d983ab339441df433b505
wuyuwangyu <1738314065@qq.com>
2025-10-29 09:06:31 +0800

    完善跨域问题

--- backend/src/main/java/com/second/hand/trading/server/WebMvcConfig.java
+++ backend/src/main/java/com/second/hand/trading/server/WebMvcConfig.java
@@ -17,7 +17,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")  // 可限制哪个请求可以通过跨域
                 .allowedHeaders("*")  // 可限制固定请求头可以通过跨域
                 .allowedMethods("*") // 可限制固定methods可以通过跨域
-                .allowedOrigins("http://localhost:8081","http://localhost:8080","http://192.168.0.100:8081")  // 可限制访问ip可以通过跨域
+                .allowedOrigins("http://localhost:8081","http://localhost:8080","http://192.168.0.100:8081","http://localhost:9090")  // 可限制访问ip可以通过跨域
                 .allowCredentials(true) // 是否允许发送cookie
                 .exposedHeaders(HttpHeaders.SET_COOKIE);
     }
