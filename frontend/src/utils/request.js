ce446ec10d66c67b857fc455d62130c8969f885c
wuyuwangyu <1738314065@qq.com>
2025-11-15 14:46:35 +0800

    请求拦截

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -11,6 +11,24 @@ const service = axios.create({
 // request interceptor(请求拦截器)
 service.interceptors.request.use(
     config => {
+        // 添加认证token到请求头
+        const token = localStorage.getItem('token') || sessionStorage.getItem('token');
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`;
+        }
+        // 设置Content-Type
+        if (!config.headers['Content-Type']) {
+            config.headers['Content-Type'] = 'application/json;charset=UTF-8';
+        }
+
+        // 添加时间戳防止缓存
+        if (config.method === 'get') {
+            config.params = {
+                ...config.params,
+                _t: Date.now()
+            };
+        }
+
         return config;
     },
     error => {
